400d42e403667fe3f3927d7b729b96f5ab5e3746
yykdev <assavictory@gmail.com>
2019-07-13 16:26:00 +0900

    브라우저 url query 추출하여 활용 해보기

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,5 +6,12 @@ export default function About({ location }) {
     // console.log(searchParams.get("name"));
     const query = queryString.parse(location.search);
     console.log(query);
-    return <div>About 입니다.</div>;
+    const name = query.name;
+
+    return (
+        <div>
+            <h2>About 입니다.</h2>
+            {name && <p>이름은 {name} 입니다.</p>}
+        </div>
+    );
 }
