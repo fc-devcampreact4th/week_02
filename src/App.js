f958cb97ba380a7e08fe814e6e4460e44a476366
yykdev <assavictory@gmail.com>
2019-07-13 16:32:58 +0900

    Route Switch 문

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
@@ -8,12 +8,12 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <switch>
+                <Switch>
                     <Route path="/profile/:id" component={Profile} />
                     <Route path="/profile" component={Profile} />
                     <Route path="/about" component={About} />
                     <Route path="/" component={Home} />
-                </switch>
+                </Switch>
             </BrowserRouter>
         </div>
     );
