15eb2d4e7d5d5199575007a7a1184bc932cef652
yykdev <assavictory@gmail.com>
2019-07-13 16:08:35 +0900

    BrowserRouter 를 이용한 기본 라우팅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
@@ -6,9 +7,11 @@ import About from "./pages/About";
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Profile />
-      <About />
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/about" component={About} />
+      </BrowserRouter>
     </div>
   );
 }
