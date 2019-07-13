ba5037d046c7eaf850763294684b176f52d7df4b
yykdev <assavictory@gmail.com>
2019-07-13 16:35:13 +0900

    React 에서의 링크 활용하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
@@ -8,6 +8,23 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <ul>
+                    <li>
+                        <Link to="/">/</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">/profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile/1">/profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">/about</Link>
+                    </li>
+                    <li>
+                        <Link to="/about?name=Kim">/about</Link>
+                    </li>
+                </ul>
                 <Switch>
                     <Route path="/profile/:id" component={Profile} />
                     <Route path="/profile" component={Profile} />
