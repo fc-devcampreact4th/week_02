5bad6e12f022eae817cf3f415343b81e0329c48d
yykdev <assavictory@gmail.com>
2019-07-13 16:23:55 +0900

    브라우저 url query 추출하기

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import queryString from "query-string";
 
-export default function About() {
+export default function About({ location }) {
+    // const searchParams = new URLSearchParams(location.search);
+    // console.log(searchParams.get("name"));
+    const query = queryString.parse(location.search);
+    console.log(query);
     return <div>About 입니다.</div>;
 }
